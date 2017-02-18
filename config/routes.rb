42174d61a851df8573fc36634efd240b9eb0cc72
jerome <fanniojerome@free.fr>
2017-02-18 09:26:35 +0100

    envoi github

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    "articles",          to: "articles#index"
+
+  get    "/:id",              to: "articles#show"
+
+  get    "new",               to: "articles#new"
+  post   "articles",          to: "articles#create"
+
+  get    "articles/:id/edit", to: "articles#edit"
+  patch  "articles/:id",      to: "articles#update"
+
+  delete "articles/:id",      to: "articles#destroy"
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
